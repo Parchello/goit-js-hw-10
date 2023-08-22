7a48aa0a2d49edbb3163956dd42a93de180fdb0d
Parchello <129171831+Parchello@users.noreply.github.com>
2023-08-22 13:26:04 +0000

    Deploying to gh-pages from @ Parchello/goit-js-hw-10@a9ca2f748057a7c26c87f6b3d66f84d02546db5b 🚀

---
+++ index.91e935a7.js
@@ -0,0 +1,2 @@
+[document.querySelector(".breed-select"),document.querySelector(".cat-info")][0].addEventListener("select",(function(e){console.dir(e.currentTarget)}));
+//# sourceMappingURL=index.91e935a7.js.map
